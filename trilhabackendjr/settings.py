b35ab97bc72d175146666ace80f759ea4241e2ef
gentil <gentilrn.65@hotmail.com>
2024-07-06 23:01:59 +1000

    Add runtime.txt

--- trilhabackendjr/settings.py
+++ trilhabackendjr/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-xe*)4!#aqg-!*y-(anl+_g3nnis(jo_!f0hsl89g7#*__t)_2*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,10 +125,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR,  "static")
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
